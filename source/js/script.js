bf76a6220ed040d2c3e00e45f28779e7f4ee70bf
Mellina05 <meidipeng2018@u.northwestern.edu>
2017-05-29 00:30:21 -0500

    new tree, understand part in progress

--- source/js/script.js
+++ source/js/script.js
@@ -3,17 +3,25 @@ function show_content(id){
   		
   		len = id.toString().length;
   		parent_id = id.toString().substring(0,id.toString().length-2);
+
   		console.log("Remove content of id(parent): " + parent_id);
-  		document.getElementById(parent_id).style.display='none';
+      if(document.getElementById(parent_id))
+  		  document.getElementById(parent_id).style.display='none';
   		button_id = parent_id + "_button";
-  		document.getElementsByClassName(button_id)[0].style.display='none';
+      if(document.getElementsByClassName(button_id))
+  		  document.getElementsByClassName(button_id)[0].style.display='none';
   		this_button_id = id + "_button";
-  		document.getElementsByClassName(this_button_id)[0].style.display='block';
-      grid_id = id + "_grid";
-      console.log("Final Node: ", grid_id);
-      document.getElementById(grid_id).style.display='block';
-      // this_button_id = id + "_button";
-      // document.getElementsByClassName(this_button_id)[0].style.display='block'
+      if(document.getElementsByClassName(this_button_id))
+  		  document.getElementsByClassName(this_button_id)[0].style.display='block';
+
+      recommendation_id = parent_id + "_recommendation";
+      this_recommendation_id = id + "_recommendation";
+      
+      if(document.getElementById(this_recommendation_id))
+        document.getElementById(this_recommendation_id).style.display='block';
+
+      if(document.getElementById(recommendation_id))
+        document.getElementById(recommendation_id).style.display='none';
   	}
 
 function return_content(id){
@@ -26,52 +34,54 @@ function return_content(id){
       document.getElementsByClassName(button_id)[0].style.display='block';
       this_button_id = id + "_button";
       document.getElementsByClassName(this_button_id)[0].style.display='none';
-      grid_id = id + "_grid";
-      document.getElementById(grid_id).style.display='none';
+      
+      recommendation_id = parent_id + "_recommendation";
+      this_recommendation_id = id + "_recommendation";
+
+      if(document.getElementById(this_recommendation_id))
+        document.getElementById(this_recommendation_id).style.display='none';
+
+      if(document.getElementById(recommendation_id))
+        document.getElementById(recommendation_id).style.display='block';
     }
 
 function showCheck(name){
   		boxes = document.getElementsByClassName(name);
-      var section_list = []
-  		if(boxes[0].checked){ // ST
-  			if(boxes[1].checked){ //phone and add
-  				if(boxes[2].checked){ // email and OT
-            show_content('1_1_1_3');
-            // section_list.push("node_3");
+      if(boxes[0].checked){
+        show_content("1_0");
+      }
+      else{
+        if(boxes[1].checked){ // ST
+          if(boxes[2].checked){ //phone and add
+            if(boxes[3].checked){ // email and OT
+              // lead to acquisition
+              window.alert("Going to acquisition part");
+              window.location.href='acquisition.html';
+            }
+            else{
+              show_content('1_2');
+              //document.getElementById("demographic_seg").style.display = 'block';
+            }
           }
           else{
-            // show_content('1_1_1_2');  
-            // st and pn add selected
-            section_list.push("demographic_seg");
+            show_content('1_1');
+            //document.getElementById("data_analysis").style.display = 'block';
           }
         }
-        else{
-          // st selected
-          //show_content('1_1_1_1');
-          section_list.push("data_analysis");
+        if(boxes[4].checked){ // sm
+          show_content('1_4');
+          document.getElementById("social_listening").style.display = 'block';
         }
-      }
-      if(boxes[3].checked){ // sm
-        section_list.push("social_listening");
-      }
-      if(boxes[4].checked){ //webact
-        section_list.push("website_analysis");
-      }
-      if(boxes[5].checked){  // app act
-        section_list.push("app_analysis");
-      }
-      
-      node_section(section_list);
-      
-}
+        if(boxes[5].checked){ //webact
+          show_content('1_5');
+          document.getElementById("website_analysis").style.display = 'block';
+        }
+        if(boxes[6].checked){  // app act
+          show_content('1_6');
+          document.getElementById("app_analysis").style.display = 'block';
+        }    
 
-function node_section(section_list){
-  if(section_list.length != 0){
-        console.log(section_list);
-        localStorage.setItem('section_list', section_list);
-        window.location.href = 'node_section.html';
-        // node_section(section_list);
-        return;
-  }
-
+      }
+     
+  		
 }
